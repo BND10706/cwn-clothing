1df3e7c3e92102da30779bc0f0a0aad2c23ea322
Brandon <BND10706@aol.com>
2020-02-01 00:11:11 -0500

    cart icon added, and cart dropdown components to app, implemented reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,30 +1,44 @@
 import React from "react";
-import './header.styles.scss'
+import "./header.styles.scss";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
-import {auth} from '../../firebase/firebase.utils';
-import { connect } from 'react-redux';
+import { auth } from "../../firebase/firebase.utils";
+import { connect } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({currentUser}) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
-    <Link className='logo-container' to="/">
+    <Link className="logo-container" to="/">
       <Logo className="logo" />
     </Link>
     <div className="options">
+      <Link className="option" to="/shop">
+        Shop
+      </Link>
+      <Link className="option" to="/shop">
+        Contact
+      </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          Sign Out
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          Sign In
+        </Link>
+      )}
+      <CartIcon/>
     </div>
-        <Link className='option' to='/shop'>Shop</Link>
-        <Link className='option' to='/shop'>Contact</Link>
-        {
-          currentUser ? 
-          <div className='option' onClick ={() => auth.signOut()}>Sign Out</div> 
-          : 
-          <Link className='option' to='/signin'>Sign In</Link>
-        }
+    {
+      hidden ? null : <CartDropdown/> 
+    }
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
-})
+const mapStateToProps = ({user: {currentUser}, cart: {hidden} }) => ({
+  currentUser,
+  hidden
+});
 
 export default connect(mapStateToProps)(Header);
